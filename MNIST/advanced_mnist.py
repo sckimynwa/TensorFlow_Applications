04de90d9f2ccdade564d5aa78636d00393a8cc26
user.email <tsb03672@naver.com>
2020-02-16 22:17:09 +0900

    91% accuracy

--- MNIST/advanced_mnist.py
+++ MNIST/advanced_mnist.py
@@ -1,16 +1,33 @@
-# download MNIST Datasets
+# download mnist examples
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
+# loading
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-# import tensorflow library
-import tensorflow as tf
-
-# set variables
-x = tf.placeholder(tf.float32, [None 784])
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros[10]))
+# Set model
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-# cross-entropy
 y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
+# Session
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# Learning
+for i in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# Validation
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Result
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
